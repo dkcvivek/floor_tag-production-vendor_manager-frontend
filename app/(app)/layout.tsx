d62bb4aa3a92fff813c99d80a03233f6d46167e2
dkcvivek <vivek.jha@dkcexport.co.in>
2026-01-22 10:41:16 +0530

    Created SubHeader for inner pages

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
+import SubHeader from "../components/SubHeader";
 
 function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen">
       <main className="pt-16">
+        <SubHeader header="CREATE CHECKER ACCOUNT"/>
         {children}
       </main>
     </div>

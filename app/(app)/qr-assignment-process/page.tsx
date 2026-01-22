8f9b94dffd505c5476fdebdc2b6d25d4eb5db686
dkcarjit <arjit.anand@dkcexport.co.in>
2026-01-22 15:43:49 +0530

    Created Loader and 404 not found page

--- app/(app)/qr-assignment-process/page.tsx
+++ app/(app)/qr-assignment-process/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
 
   return (
     <>
-      <Navbar />
+      <Navbar title="QR ASSIGNMENT PROCESS" />
       <div
         className={`min-h-dvh w-full bg-[#f5f7fb] flex justify-center px-3 py-4 transition-all ${
           modal ? "blur-sm" : ""
